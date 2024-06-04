a3848ab481368627972685981b06b5dd9d010f59
Fatih AKTAŞ <132843565+fatihaktas845@users.noreply.github.com>
2024-06-04 14:24:18 +0300

    Some changing

--- omni_ADT1061.mk
+++ omni_ADT1061.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := m20lte
+PRODUCT_RELEASE_NAME := ADT1061
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m20lte
-PRODUCT_NAME := omni_m20lte
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := ADT1061
+PRODUCT_NAME := omni_ADT1061
+PRODUCT_BRAND := aidata
+PRODUCT_MANUFACTURER := aidata
