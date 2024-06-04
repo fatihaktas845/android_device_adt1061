3bac2612d07b49de5a80db962dc742eba18d78e4
Fatih AKTAŞ <132843565+fatihaktas845@users.noreply.github.com>
2024-06-04 21:45:51 +0300

    Update omni_ADT1061.mk

--- omni_ADT1061.mk
+++ omni_ADT1061.mk
@@ -1,14 +1,18 @@
-# Release name
-PRODUCT_RELEASE_NAME := ADT1061
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit from device
+$(call inherit-product, device/aidata/ADT1061/device.mk)
 
-# Inherit from our custom product configuration
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ADT1061
 PRODUCT_NAME := omni_ADT1061
 PRODUCT_BRAND := aidata
+PRODUCT_MODEL := ADT1061
 PRODUCT_MANUFACTURER := aidata
+PRODUCT_RELEASE_NAME := aidata ADT1061
