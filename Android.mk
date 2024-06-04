71d4b630c8b930f4842d04a3cd8207843c1a5bc2
Fatih AKTAŞ <132843565+fatihaktas845@users.noreply.github.com>
2024-06-04 21:05:05 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
-ifneq ($(filter ADT1061,$(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+ifeq ($(TARGET_DEVICE), ADT1061)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
